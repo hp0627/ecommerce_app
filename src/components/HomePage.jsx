813d1adf9e14586984f263dc774ad254c635fb3b
hp0627 <harshapriya0627@gmail.com>
2025-08-21 12:28:02 +0530

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
     <div className="container">
       {/* Header */}
       <div className="header">
-        <h1>E-Commerce(SKILL 9)</h1>
+        <h1>E-Commerce(SKILL 9,10)</h1>
         <div className="header-right">
           {isAuthenticated ? (
             <div className="profile-menu">
